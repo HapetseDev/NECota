b25b0948d6c3617e4a18ccc8f2bd85073a79d473
HapetseDev <151859270+HapetseDev@users.noreply.github.com>
2025-03-13 21:31:00 +0100

    Animation geaddet

--- Skripte/General/GameConstants.cs
+++ Skripte/General/GameConstants.cs
@@ -1,11 +1,30 @@
 using Godot;
 using System;
 
+/* 
+ * This class is used to store all the constants used in the game.
+ * This is a good practice to keep all the constants in one place so that they can be easily accessed and modified.
+ * This class is inherited from Node class so that it can be added to the scene tree and accessed from any script.
+ * 
+ * This class contains the following constants:
+ * 1. Animations: This class contains the names of the animations used in the game.
+ * 2. Input: This class contains the names of the input actions used in the game.
+ * 
+ * This class is used in the following scripts:
+ * 1. Player.cs
+ * 2. PlayerMovement.cs
+ * 3. PlayerAnimation.cs
+ * 4. PlayerInput.cs
+ * 
+ * This class is added to the scene tree in the following scenes:
+ * 1. Player.tscn
+ */
+ 
 public partial class GameConstants : Node
 {
 	// Animations
 	public const string ANIM_IDLE = "Idle";
-	public const string ANIM_MOVE = "Move";
+	public const string ANIM_MOVE = "Moving";
 	
 	//Input
 	public const string INPUT_MOVE_LEFT = "MoveLeft";
--- Skripte/General/kampfszene.gd
+++ Skripte/General/kampfszene.gd
@@ -4,6 +4,7 @@ var player_turn = true
 var player_hp = 100
 var enemy_hp = 50
 
+# Called when the node enters the scene tree for the first time.
 func _on_attack_button_pressed():
 	if player_turn:
 		enemy_hp -= 20
